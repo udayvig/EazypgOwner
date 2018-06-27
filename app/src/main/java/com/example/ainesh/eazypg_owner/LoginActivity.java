068b5744d245abfee0565cbe218e389e24eda316
Vardaan <vardaan13121997@gmail.com>
2018-06-27 15:04:52 +0530

    Merge branch 'master' of C:\Users\Vardaan\AndroidStudioProjects\EazypgOwner1 with conflicts.

--- app/src/main/java/com/example/ainesh/eazypg_owner/LoginActivity.java
+++ app/src/main/java/com/example/ainesh/eazypg_owner/LoginActivity.java
@@ -5,11 +5,9 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,43 +24,31 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
 
-    private int RC_SIGN_IN=1;
+    private int RC_SIGN_IN = 1;
 
     private EditText etUserEmail,etUserPassword;
-    private ImageView btnSignIn;
+    private Button btnSignIn;
     private TextView loginToSignIn;
 
     private String userEmail, userPassword;
-
-
-    @Override
-    public void onBackPressed() {
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_HOME);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//***Change Here***
-        startActivity(intent);
-        finish();
-        System.exit(0);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginToSignIn=(TextView) findViewById(R.id.tvLoginToSignUp);
-        btnSignIn=(ImageView) findViewById(R.id.btnSignIn);
-        etUserEmail=(EditText) findViewById(R.id.usernametextView);
-        etUserPassword=(EditText) findViewById(R.id.passwordtextView);
+        loginToSignIn = findViewById(R.id.tvLoginToSignUp);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        etUserEmail = findViewById(R.id.usernametextView);
+        etUserPassword = findViewById(R.id.passwordtextView);
 
-        etUserEmail.requestFocus(1);
         mFirebaseAuth=FirebaseAuth.getInstance();
         mFirebaseUser=mFirebaseAuth.getCurrentUser();
 
         //If user kills the app without logging out, he/she should need not to login again.
         if(mFirebaseUser!=null){
             finish();
-            startActivity(new Intent(LoginActivity.this,HomePageActivity.class));
+            startActivity(new Intent(LoginActivity.this,DummyActivity.class));
         }
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
@@ -70,31 +56,18 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 userEmail=etUserEmail.getText().toString().trim();
                 userPassword=etUserPassword.getText().toString().trim();
-                    if(!(userPassword.isEmpty()) && !(userEmail.isEmpty())) {
-                        mFirebaseAuth.signInWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    startActivity(new Intent(LoginActivity.this, HomePageActivity.class));
-                                    Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(LoginActivity.this, "Login Failed.", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-                    }
-                    else {
-                        if(userEmail.isEmpty()) {
 
-                            etUserEmail.setError("Field cannot be empty");
-                        }
-                        if(userPassword.isEmpty()){
-
-                            etUserPassword.setError("Field cannot be empty");
-
+                mFirebaseAuth.signInWithEmailAndPassword(userEmail,userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            startActivity(new Intent(LoginActivity.this,DummyActivity.class));
+                            Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(LoginActivity.this, "Login Failed.", Toast.LENGTH_SHORT).show();
                         }
                     }
-
+                });
             }
         });
 
@@ -102,7 +75,6 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this,SignupActivity.class));
-                finish();
             }
         });
     }
