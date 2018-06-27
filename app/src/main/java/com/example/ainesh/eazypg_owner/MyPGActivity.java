25ac9abadd4e70cb21153dd4468255abf9afecf4
Vardaan <vardaan13121997@gmail.com>
2018-06-27 16:29:42 +0530

    My PG Activity Edit

--- app/src/main/java/com/example/ainesh/eazypg_owner/MyPGActivity.java
+++ app/src/main/java/com/example/ainesh/eazypg_owner/MyPGActivity.java
@@ -35,11 +35,37 @@ public class MyPGActivity extends AppCompatActivity {
     EditText pgNameEditText;
     EditText bioEditText;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_pg);
+
+        noOfBathEditText = findViewById(R.id.noOfBathEditText);
+        noOfRoomsEditText = findViewById(R.id.noOfRoomsEditText);
+        staffNumberEditText = findViewById(R.id.staffNumberEditText);
+        occupancyEditText = findViewById(R.id.occupancyEditText);
+        genderEditText = findViewById(R.id.genderEditText);
+        locationEditText = findViewById(R.id.locationEditText);
+        lastEntryEditText = findViewById(R.id.lastEntryEditText);
+        pgEmailEditText = findViewById(R.id.pgEmailEditText);
+        distLandEditText = findViewById(R.id.distLandEditText);
+        ownerNameEditText = findViewById(R.id.ownerNameEditText);
+        contactEditText = findViewById(R.id.contactEditText);
+        pgNameEditText = findViewById(R.id.pgNameEditText);
+        bioEditText = findViewById(R.id.bioEditText);
+
+        bathroomButton = findViewById(R.id.bathroomButton);
+        bioButton = findViewById(R.id.bioButton);
+        pgNameButton = findViewById(R.id.pgNameButton);
+        roomsButton = findViewById(R.id.roomsButton);
+        staffCountButton = findViewById(R.id.staffCountButton);
+        landDistButton = findViewById(R.id.landDistButton);
+        lastEntryButton = findViewById(R.id.lastEntryButton);
+        emailButton = findViewById(R.id.mailButton);
+        genderButton = findViewById(R.id.genderButton);
+        occupancyButton = findViewById(R.id.occupancyButton);
+        contactButton = findViewById(R.id.contactButton);
+        ownerNameButton = findViewById(R.id.ownerNameButton);
+        locationButton = findViewById(R.id.locationButton);
     }
 }
