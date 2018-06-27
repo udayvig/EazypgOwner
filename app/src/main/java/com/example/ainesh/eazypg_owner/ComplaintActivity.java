742ce56a015de0c57939b099d737728d4b891cea
Ainesh Chhetri <ainesh3098@gmail.com>
2018-06-27 15:02:27 +0530

    27/6/18

---
+++ app/src/main/java/com/example/ainesh/eazypg_owner/ComplaintActivity.java
@@ -0,0 +1,13 @@
+package com.example.ainesh.eazypg_owner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComplaintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaint);
+    }
+}
--- app/src/main/java/com/example/ainesh/eazypg_owner/HomePageActivity.java
+++ app/src/main/java/com/example/ainesh/eazypg_owner/HomePageActivity.java
@@ -1,5 +1,6 @@
 package com.example.ainesh.eazypg_owner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,23 +21,29 @@ public class HomePageActivity extends AppCompatActivity {
             @Override
             public void onItemClick(CircleMenuButton menuButton) {
                 switch (menuButton.getId()) {
-                    case R.id.favorite:
-                        showMessage("Favorite");
+                    case R.id.pgView:
+                        showMessage("YOUR PG");
+                        startActivity(new Intent(HomePageActivity.this,MyPGActivity.class));
                         break;
-                    case R.id.search:
-                        showMessage("Search");
+                    case R.id.tenantView:
+                        showMessage("TENANTS");
+                        startActivity(new Intent(HomePageActivity.this,TenantActivity.class));
                         break;
-                    case R.id.alert:
-                        showMessage("Alert");
+                    case R.id.staffView:
+                        showMessage("STAFF");
+                        startActivity(new Intent(HomePageActivity.this,StaffActivity.class));
                         break;
-                    case R.id.tenant:
-                        showMessage("Tenant");
+                    case R.id.complainView:
+                        showMessage("COMPLAINTS");
+                        startActivity(new Intent(HomePageActivity.this,ComplaintActivity.class));
                         break;
-                    case R.id.PG:
-                        showMessage("PG Details");
+                    case R.id.subscriptionView:
+                        showMessage("SUBSCRIPTIONS");
+                        startActivity(new Intent(HomePageActivity.this,SubscriptionActivity.class));
                         break;
-                    case R.id.Subscriptions:
-                        showMessage("Subscriptions");
+                    case R.id.cashflowView:
+                        showMessage("CASHFLOW");
+                        startActivity(new Intent(HomePageActivity.this,CashflowActivity.class));
                 }
             }
         });
