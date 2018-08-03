d4fd1fab30a284df10083183b642548f07bdab8c
Vardaan <vardaan13121997@gmail.com>
2018-08-04 04:30:12 +0530

    Complaints Completed

---
+++ app/src/main/java/com/example/EazyPG/owner/DetailsClasses/ComplaintDetails.java
@@ -0,0 +1,28 @@
+package com.example.EazyPG.owner.DetailsClasses;
+
+public class ComplaintDetails {
+    public String feedback, name, room, date, firstLevel, secondLevel, thirdLevel, newId;
+    public boolean resolved;
+
+    public ComplaintDetails(String feedback, String name, String room, String date, String firstLevel, String secondLevel, String thirdLevel, boolean resolved, String newId) {
+        this.feedback = feedback;
+        this.name = name;
+        this.room = room;
+        this.date = date;
+        this.firstLevel = firstLevel;
+        this.secondLevel = secondLevel;
+        this.thirdLevel = thirdLevel;
+        this.resolved = resolved;
+        this.newId = newId;
+    }
+
+    public ComplaintDetails(String feedback, String date, String firstLevel, String secondLevel, String thirdLevel, String newId, boolean resolved) {
+        this.feedback = feedback;
+        this.date = date;
+        this.firstLevel = firstLevel;
+        this.secondLevel = secondLevel;
+        this.thirdLevel = thirdLevel;
+        this.newId = newId;
+        this.resolved = resolved;
+    }
+}
