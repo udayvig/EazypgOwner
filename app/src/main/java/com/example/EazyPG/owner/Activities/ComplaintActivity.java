a3a05cec993be8e2d7074128bfd3a16f589d3d30
Vardaan <vardaan13121997@gmail.com>
2018-08-04 00:40:17 +0530

    03/08/2018

--- app/src/main/java/com/example/EazyPG/owner/Activities/ComplaintActivity.java
+++ app/src/main/java/com/example/EazyPG/owner/Activities/ComplaintActivity.java
@@ -2,6 +2,8 @@ package com.example.EazyPG.owner.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 import com.example.ainesh.eazypg_owner.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -11,17 +13,46 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class ComplaintActivity extends AppCompatActivity {
 
-    FirebaseAuth firebaseAuth;
-    FirebaseUser firebaseUser;
-    FirebaseDatabase firebaseDatabase;
-    DatabaseReference databaseReference;
+    CardView bedroomComplaint, foodComplaint, facilityComplaint, securityComplaint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
 
-        
+        bedroomComplaint = findViewById(R.id.bedroomComplaint);
+        foodComplaint = findViewById(R.id.foodComplaint);
+        facilityComplaint = findViewById(R.id.facilityComplaint);
+        securityComplaint = findViewById(R.id.securityComplaint);
 
+        bedroomComplaint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
+
+        foodComplaint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        facilityComplaint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        securityComplaint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
