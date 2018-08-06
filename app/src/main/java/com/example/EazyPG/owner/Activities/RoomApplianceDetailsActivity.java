72a34d8c1b7f3b44da28d83eb031567a6537604c
Ainesh Chhetri <ainesh3098@gmail.com>
2018-08-06 17:40:32 +0530

    complaint row

---
+++ app/src/main/java/com/example/EazyPG/owner/Activities/RoomApplianceDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.example.EazyPG.owner.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ainesh.eazypg_owner.R;
+
+public class RoomApplianceDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_appliance_details);
+    }
+}
