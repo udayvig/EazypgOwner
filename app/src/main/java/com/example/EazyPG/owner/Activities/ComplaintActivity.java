b29427e6d6c2eb5f5fc291d08c5e2cf9f60768ef
Vardaan <vardaan13121997@gmail.com>
2018-08-19 14:20:24 +0530

    19-08-2018

--- app/src/main/java/com/example/EazyPG/owner/Activities/ComplaintActivity.java
+++ app/src/main/java/com/example/EazyPG/owner/Activities/ComplaintActivity.java
@@ -1,20 +1,32 @@
 package com.example.EazyPG.owner.Activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
 
+import com.example.EazyPG.owner.DetailsClasses.ComplaintDetails;
 import com.example.ainesh.eazypg_owner.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ComplaintActivity extends AppCompatActivity {
 
     CardView bedroomComplaint, foodComplaint, facilityComplaint, securityComplaint;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    int countBedroom, countFood, countFacility, countSecurity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,73 @@ public class ComplaintActivity extends AppCompatActivity {
         foodComplaint = findViewById(R.id.foodComplaint);
         facilityComplaint = findViewById(R.id.facilityComplaint);
         securityComplaint = findViewById(R.id.securityComplaint);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        countBedroom = countFacility = countSecurity = countFood = 0;
+
+        databaseReference = firebaseDatabase.getReference("PG/" + firebaseUser.getUid() + "/Complaints");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    ComplaintDetails complaintDetails = snapshot.child("Bedroom").getValue(ComplaintDetails.class);
+
+                    if (!complaintDetails.resolved) {
+
+                        countBedroom ++;
+                    }
+
+                }
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    ComplaintDetails complaintDetails = snapshot.child("Food").getValue(ComplaintDetails.class);
+
+                    if (!complaintDetails.resolved) {
+
+                        countFood ++;
+                    }
+
+                }
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    ComplaintDetails complaintDetails = snapshot.child("Facility").getValue(ComplaintDetails.class);
+
+                    if (!complaintDetails.resolved) {
+
+                        countFacility ++;
+                    }
+
+                }
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    ComplaintDetails complaintDetails = snapshot.child("Security").getValue(ComplaintDetails.class);
+
+                    if (!complaintDetails.resolved) {
+
+                        countSecurity ++;
+                    }
+
+                }
+
+                // Add counts to bubbles
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         bedroomComplaint.setOnClickListener(new View.OnClickListener() {
             @Override
