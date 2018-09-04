927c5fc0a4479a41393fdf3fb48e8952be73a335
Vardaan <vardaan13121997@gmail.com>
2018-09-04 18:37:26 +0530

    Appliance Start

--- app/src/main/java/com/example/EazyPG/owner/Activities/ApplianceActivity.java
+++ app/src/main/java/com/example/EazyPG/owner/Activities/ApplianceActivity.java
@@ -1,46 +1,200 @@
 package com.example.EazyPG.owner.Activities;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.ainesh.eazypg_owner.R;
 
-public class ApplianceActivity extends AppCompatActivity{
+public class ApplianceActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
-    private Button addBtn ;
-    private View view2;
-
-    public String[] appliances={"AC", "CCTV", "D2H", "Dishwasher", "Fan", "Geyser", "Heater", "Induction",
-            "Iron", "Lift", "Microwave", "Other", "Refrigerator", "RO", "Router", "TV", "Washing Machine"};
+    Button acButton, cctvButton, d2hButton, dishwasherButton, fanButton, geyserButton, heaterButton, inductionButton, ironButton, liftButton, microwaveButton, refrigeratorButton, roButton, routerButton, tvButton, washingMachineButton, otherButton;
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appliance);
 
-        toolbar = findViewById(R.id.applianceToolbar);
-        setSupportActionBar(toolbar);
+        acButton = findViewById(R.id.acButton);
+        cctvButton = findViewById(R.id.cctvButton);
+        d2hButton = findViewById(R.id.d2hButton);
+        dishwasherButton = findViewById(R.id.dishwasherButton);
+        fanButton = findViewById(R.id.fanButton);
+        geyserButton = findViewById(R.id.geyserButton);
+        heaterButton = findViewById(R.id.heaterButton);
+        inductionButton = findViewById(R.id.inductionButton);
+        ironButton = findViewById(R.id.ironButton);
+        liftButton = findViewById(R.id.liftButton);
+        microwaveButton = findViewById(R.id.microwaveButton);
+        refrigeratorButton = findViewById(R.id.refrigeratorButton);
+        roButton = findViewById(R.id.roButton);
+        routerButton = findViewById(R.id.routerButton);
+        tvButton = findViewById(R.id.tvButton);
+        washingMachineButton = findViewById(R.id.washingMachineButton);
+        otherButton = findViewById(R.id.otherButton);
 
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        RecyclerView mAppliancesList = findViewById(R.id.rv);
+        acButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "AC");
+                startActivity(intent);
+                finish();
+            }
+        });
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        mAppliancesList.setLayoutManager(layoutManager);
+        cctvButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "CCTV");
+                startActivity(intent);
+                finish();
+            }
+        });
 
-        mAppliancesList.setHasFixedSize(true);
+        dishwasherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "Dishwasher");
+                startActivity(intent);
+                finish();
+            }
+        });
 
-        ApplianceAdapter adapter = new ApplianceAdapter(appliances, getLayoutInflater(),null);
+        fanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "Fan");
+                startActivity(intent);
+                finish();
+            }
+        });
 
-        mAppliancesList.setAdapter(adapter);
+        geyserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "Geyser");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        heaterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "Heater");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        inductionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "Induction");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        ironButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "Iron");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        liftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "Lift");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        microwaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "Microwave");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        refrigeratorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "Refrigerator");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        roButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "RO");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        routerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "Router");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        tvButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "TV");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        washingMachineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "Washing Machine");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        otherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ApplianceActivity.this, ApplianceDetailsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, "Other");
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
-
-
-
 }
